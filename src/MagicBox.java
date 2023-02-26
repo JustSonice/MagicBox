d3709a936c255148826f14127ded7bd93f84c78a
Stanislav Chagin <windowsme5@gmail.com>
2023-02-26 15:02:17 +0300

    Logic bug fixes. Final release v 2.0

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -11,7 +11,7 @@ public class MagicBox<T> {
 
     public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
-            if (items[i] == null | items[i] != null & size > items.length) {
+            if (items[i] == null) {
                 items[i] = item;
                 System.out.println("True значение " + item + " записано");
                 return true;
@@ -23,15 +23,13 @@ public class MagicBox<T> {
 
     public T pick() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i] != null & items.length == size) {
+            if (items[i] != null) {
                 Random random = new Random();
                 int randomInt = random.nextInt(size);
-                System.out.println(items[randomInt]);
+                return (items[randomInt]);
             }
-            else {
-                int k = items.length - 1;
-                throw new RuntimeException("Ошибка, коробка не полна. Осталоcь заполнить:" + k);
-            }
-        } return null;
+        }
+        int k = size;
+        throw new RuntimeException("Ошибка, коробка не полна. Осталоcь заполнить:" + k);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        MagicBox <Integer> box1 = new MagicBox <>(2);
-        MagicBox <String> box2 = new MagicBox <>(2);
+        MagicBox<Integer> box1 = new MagicBox<>(2);
+        MagicBox<String> box2 = new MagicBox<>(2);
         box1.add(2);
         box1.add(1);
         box1.add(5);
